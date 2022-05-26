ac03c92b0aa23b80ea24c6f2457b496de19ce0cd
Aden Ali <aden_ali@hotmail.com>
2022-05-26 15:34:19 +0100

    latest js

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,26 @@ $(document).ready(function () {
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-        
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+
+    
 
 })
